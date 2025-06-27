1468bf755cae3aea7ece61fc9ac39ed32aea2162
Murad Imanov <imanovmurad896@gmail.com>
2025-06-27 12:37:32 +0300

    fix: 🐛 changed hardcoded file names to absolute path

--- week-5/adapter/task-2.js
+++ week-5/adapter/task-2.js
@@ -23,7 +23,7 @@ const fs = require('node:fs');
 const read = promisify(fs.readFile);
 
 const main = async () => {
-  const fileName = '1-promisify.js';
+  const fileName = __filename;
   const data = await read(fileName, 'utf8');
   console.log(`File "${fileName}" size: ${data.length}`);
 };
